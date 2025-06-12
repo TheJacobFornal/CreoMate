03a79d6e6e22049f209b516fb6197c34a30c6bc2
Jakub  Fornal <jakub.fornal@pemes.pl>
2025-06-12 14:33:57 +0200

    tree update page

--- frontend/src/SideMenu/SideMenu.jsx
+++ frontend/src/SideMenu/SideMenu.jsx
@@ -2,6 +2,7 @@ import React from 'react';
 import zakupy from '../assets/zakupy.png';
 import constructor from '../assets/constructors.png';
 import setttings from '../assets/settings.png';
+import tree from '../assets/tree_1.png';
 import './SideMenu.css';
 
 
@@ -15,6 +16,9 @@ const SideMenu = ({ setActivePage }) => {
         <img src={zakupy} alt="Hint Icon" style={{ width: '24px', height: '24px' }} />
       </div>
       <div style={{ margin: '20px 0', cursor: 'pointer' }} title="Etap 4" onClick={() => setActivePage(4)}>
+         <img src={tree} alt="Hint Icon" style={{ width: '24px', height: '24px' }} />
+      </div>
+      <div style={{ margin: '20px 0', cursor: 'pointer' }} title="Etap 3" onClick={() => setActivePage(3)}>
          <img src={setttings} alt="Hint Icon" style={{ width: '24px', height: '24px' }} />
       </div>
     </div>
